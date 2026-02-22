121c959b7ac83e6cc9a175276c69e93ec91c026d
Sticky Pig <qiqicaosnake@gmail.com>
2026-02-22 11:05:18 -0800

    i have added a go home button

--- habitat_button.py
+++ habitat_button.py
@@ -3,5 +3,5 @@ from trees import Fih, Bord
 city_list = ["Seattle","Issaquah","Auburn","Snoqualmie","Spokane"]
 user_city = st.selectbox("Put in the city you live closest to", city_list)
 
-if st.button("Habitat Finder!"):
+if st.button("Find Habitats!"):
     st.switch_page(f"pages/{user_city}.py")
